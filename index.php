be7d3ae5cef3767f4453ab12e0c9c82731fe0982
Rasimcan Yılmaz <116042261+rasimcan@users.noreply.github.com>
2024-01-10 03:10:22 -0800

    index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>WHOIS Sorgu | kolan.net.tr</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="icon" href="img/favicon.png" type="image/png">
+    <link rel="icon" href="favicon.png" type="image/png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
@@ -62,7 +62,7 @@
 
 								echo '<br>';
 							echo '<hr>';
-                            echo '<img src="img/dark-old.png" alt="kolan bilişim teknolojileri | wwww.kolan.net.tr">';
+                            echo '<img src="dark-old.png" alt="kolan bilişim teknolojileri | wwww.kolan.net.tr">';
                         } else {
 							echo '<br>';
 							echo '<hr>';
@@ -71,7 +71,7 @@
                             echo '<font color="red"><div class="alert alert-danger"><i class="fa-solid fa-ban"></i> WHOIS bilgileri bulunamadı. </div></font>';
 							echo '<br>';
 							echo '<hr>';
-                            echo '<img src="img/dark-old.png" alt="kolan bilişim teknolojileri | wwww.kolan.net.tr">';
+                            echo '<img src="dark-old.png" alt="kolan bilişim teknolojileri | wwww.kolan.net.tr">';
 							echo '</center>';
                         }
                     }
